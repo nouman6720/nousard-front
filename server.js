bdd6cbcc9530f316b20d55ed72bbe54ee0da6b87
Muhammad Nouman <nouman6720@gmail.com.com>
2022-05-28 03:13:52 +0500

    fixs

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
 
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
 const app = express()
 
 //here we are configuring dist to serve app files
